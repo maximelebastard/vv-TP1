1e0e8097c8c5e28da972a31415e7b14837c2ab31
Maxime Lebastard <maxime.lebastard@etudiant.univ-rennes1.fr>
2015-09-17 20:14:27 +0200

    Fix bug revealed by the "set_correctElement" test

--- src/main/java/system/PhonyList.java
+++ src/main/java/system/PhonyList.java
@@ -237,7 +237,7 @@ public class PhonyList<E> {
         rangeCheck(index);
 
         E oldValue = elementData(index);
-        elementData[++index] = element;
+        elementData[index] = element;
         return oldValue;
     }
 
